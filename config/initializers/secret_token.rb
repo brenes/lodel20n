28a94338ccae907cd41092ad4d47b7ffe96ed675
David J. Brenes <davidjbrenes@gmail.com>
2011-11-20 11:05:15 +0100

    Primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lodel20n::Application.config.secret_token = '183cbf31fcf7a691edd8f94e79f9105442ae2b58ac6c26639393df958137a2fc6bd677497e621f797a3000a314b48d2eadb9fcc023c6d0006399d80f0903d1c0'
