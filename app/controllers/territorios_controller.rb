bfd46d4922ef2157ed331534b5ae1917717636e4
David J. Brenes <davidjbrenes@gmail.com>
2011-11-27 19:05:03 +0100

    Usando los decorators de Escrutinio en la vista

--- app/controllers/territorios_controller.rb
+++ app/controllers/territorios_controller.rb
@@ -2,11 +2,17 @@ class TerritoriosController < ApplicationController
 
   def index
     @territorio = Territorio.pais.first
+    @escrutinio = @territorio.ultimo_escrutinio
+    @escrutinio_html = Decorators::Escrutinio::Html.new(@escrutinio)
+    @escrutinio_seo = Decorators::Escrutinio::Seo.new(@escrutinio_html)
     render :action => :show_pais
   end
     
   def show
     @territorio = Territorio.find(params[:id])
+    @escrutinio = @territorio.ultimo_escrutinio
+    @escrutinio_html = Decorators::Escrutinio::Html.new(@escrutinio)
+    @escrutinio_seo = Decorators::Escrutinio::Seo.new(@escrutinio_html)
     render :action => :"show_#{@territorio.descripcion_tipo}"
   end
 
